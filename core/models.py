281439035f28d6b4d84195f1f321cf58f6625dcc
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-04-27 12:55:26 +0300

    profiles

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     ROLE_CHOICES =(
@@ -29,3 +31,25 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.rank} ({self.username}) - ({self.role})"
+    
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    current_class = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Student: {self.user.username} - {self.current_class}"
+    
+class InstructorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='instructor_profile')
+    subject_specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Teacher: {self.user.username} - {self.subject_specialty}"
+
+    @receiver(post_save, sender=User)    
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            if instance.role == 'student':
+                StudentProfile.objects.create(user=instance)
+            elif instance.role == 'instructor':
+                InstructorProfile.objects.create(user=instance)    
