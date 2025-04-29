5d348525e265fa093057b55fdea4575d505620ec
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-04-29 21:05:18 +0300

    Adding the result view to the admin panel

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import SchoolClass, Attendance
+from .models import SchoolClass, Attendance, Result
 
 admin.site.register(SchoolClass)
 admin.site.register(Attendance)
+
+
+@admin.register(Result)
+class ResultAdmin (admin.ModelAdmin):
+    list_display =(
+        'student',
+        'school_class',
+        'subject',
+        'remarks',
+        'date_recorded'
+    )
+    list_filter = ('school_class', 'subject', 'date_recorded')
+    search_fields = ('student__service_number', 'subject', 'school_class__name')
+    ordering = ('date_recorded',)
--- school/urls.py
+++ school/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import SchoolClassViewSet, AttendanceViewSet
+from .views import SchoolClassViewSet, AttendanceViewSet, ResultViewSet
 
 router = DefaultRouter()
 router.register(r'classes', SchoolClassViewSet, basename='schoolclass')
 router.register(r'attendance', AttendanceViewSet, basename='attendance')
+router.register(r'results', ResultViewSet, basename='results')
 
 urlpatterns = router.urls
